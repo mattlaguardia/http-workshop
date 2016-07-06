32d4cda0d1319ce1b5aba55551b62a15b81c8581
mattlaguardia <matthewlaguardia@gmail.com>
2016-07-06 14:59:19 -0700

    adds angular

--- starter-code/presidentsapp/js/presidentsController.js
+++ starter-code/presidentsapp/js/presidentsController.js
@@ -1,18 +1,31 @@
 angular.module('ThePresidentsApp', [])
   .controller('PresidentsController', PresidentsController);
 
-function PresidentsController(){
-  this.all = [
-    {name: 'George Washington', start: 1789, end: 1797 },
-    {name: 'John Adams', start: 1797, end: 1801 },
-    {name: 'Thomas Jefferson', start: 1801, end: 1809 },
-    {name: 'James Madison', start: 1809, end: 1817 }
-  ]
+PresidentsController.$inject = ['$http']; //
+
+function PresidentsController($http){
+  var self = this;
   this.addPresident = addPresident;
   this.newPresident = {};
+  this.getPresidents = getPresidents;
+  self.all = [];
 
   function addPresident(){
-    this.all.push(this.newPresident);
-    this.newPresident = {};
+    // Need to change this to do a http post //
+    // Takes two arguments request and data you are trying to send //
+    $http.post('http://localhost:3000/presidents', self.newPresident)
+      .then(function(data){
+        self.all = newPresident.data.presidents;
+      })
+    self.all.push(this.newPresident);
+    self.newPresident = {};
   }
+
+  function getPresidents() {
+    $http.get('http://localhost:3000/presidents')
+    .then(function(res){
+      self.all = res.data.presidents;
+    })
+  }
+  getPresidents();
 }
